df69232b9af88f88a722f6251625bf9d2ff703b8
Robert Z <urdxd@student.kit.edu>
2017-02-01 15:54:37 +0100

    Implementation change in follow line (unfinished).

---
+++ src/LineFollowControllSystemTest.java
@@ -0,0 +1,30 @@
+
+public class LineFollowControllSystemTest {
+
+    private double propFactor;
+    
+    public LineFollowControllSystemTest(double propFactor) {
+        this.propFactor = propFactor;
+    }
+    
+    
+    
+    public double getPropFactor() {
+        return propFactor;
+    }
+
+
+
+    public void setPropFactor(double propFactor) {
+        this.propFactor = propFactor;
+    }
+
+
+
+    public int calcSpeedLeftMotor(float targetValue, float currentValue, int speedLeft) {
+        return (int) (speedLeft + (targetValue - currentValue) * propFactor);
+    }
+    public int calcSpeedRightMotor(float targetValue, float currentValue, int speedRight) {
+        return (int) (speedRight - (targetValue - currentValue) * propFactor);
+    }
+}
--- src/LineFollowerTest.java
+++ src/LineFollowerTest.java
@@ -9,42 +9,74 @@ public class LineFollowerTest {
 	
 	private static final double targetValue = (0.55+0.3)/3;
 	
-
+	final static int STANDARD_SPEED = 240;
+	final static float TARGET_VALUE = (0.5f + 0.05f) / 2.0f;
+	
+	
 	public static void main(String[] args)
 	{
+	    // start exit Thread
 		Thread exitThread = new Thread(new exitThread.ExitThreadRunnable());
 		exitThread.start();
+		// setup motors
 		EV3LargeRegulatedMotor m_left = new EV3LargeRegulatedMotor(MotorPort.A);
 		EV3LargeRegulatedMotor m_right = new EV3LargeRegulatedMotor(MotorPort.B);
+		// setup color sensor
 		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S2);
-		SampleProvider detektor = colorSensor.getRedMode();
-		setForwardhalfMax(m_left, m_right);
+		SampleProvider detector = colorSensor.getRedMode();
 		float[] sampleSet = new float[20];
-		int circleCount = 0;
-		float speedCorrection = m_left.getMaxSpeed()/10;
+		
+		startMoving(m_left, m_right);
+        
+		int circleCount = 0; // index for the cyclic array
+		float speedCorrection = STANDARD_SPEED / 2; //m_left.getMaxSpeed()/10;
+		
+		LineFollowControllSystemTest pController = new LineFollowControllSystemTest(0.1f);
+		for (;;) {
+            detector.fetchSample(sampleSet, circleCount);
+
+            m_left.setSpeed(pController.calcSpeedLeftMotor(TARGET_VALUE, 
+                    sampleSet[circleCount], m_left.getSpeed()));
+
+            m_right.setSpeed(pController.calcSpeedLeftMotor(TARGET_VALUE, 
+                    sampleSet[circleCount], m_right.getSpeed()));
+            m_right.forward();
+            m_left.forward();
+            circleCount = ++circleCount % 20;
+        }
+		
+		/*
 		for(;;)//ever
 		{
-			detektor.fetchSample(sampleSet, circleCount);
-			float midSpeed = (m_left.getSpeed() + m_right.getSpeed())/2;
-			if (sampleSet[circleCount] >= targetValue)
+			detector.fetchSample(sampleSet, circleCount); // get sensor value
+			float midSpeed = STANDARD_SPEED; //(m_left.getSpeed() + m_right.getSpeed())/2;
+			// robot tries to stay on the left side of the bright stripe
+			if (sampleSet[circleCount] <= targetValue * 0.9f)
 			{
+			    // sensor is on the bright stripe
+			    
 				m_left.setSpeed(midSpeed+speedCorrection);
-				m_right.setSpeed(midSpeed-speedCorrection);
+				m_right.setSpeed(midSpeed);
 			}
-			else if (sampleSet[circleCount] < targetValue)
+			else if (sampleSet[circleCount] > targetValue * 1.1f)
 			{
-				m_left.setSpeed(midSpeed-speedCorrection);
+			    // sensor is over the floor
+				m_left.setSpeed(midSpeed);
 				m_right.setSpeed(midSpeed+speedCorrection);
+			} else {
+			    //sensor is on the edge 
+			    m_left.setSpeed(midSpeed);
+			    m_right.setSpeed(midSpeed);
 			}
 			m_left.forward();
 			m_right.forward();
 			circleCount = ++circleCount % 20;
 		}
-		
+		*/
 	}	
-	private static void setForwardhalfMax(EV3LargeRegulatedMotor m_left, EV3LargeRegulatedMotor m_right)
+	private static void startMoving(EV3LargeRegulatedMotor m_left, EV3LargeRegulatedMotor m_right)
 	{
-		float speed = (m_left.getMaxSpeed() + m_right.getMaxSpeed())/5;
+		float speed = STANDARD_SPEED;//(m_left.getMaxSpeed() + m_right.getMaxSpeed())/7;
 		m_left.setSpeed(speed);
 		m_right.setSpeed(speed);
 		m_left.forward();
