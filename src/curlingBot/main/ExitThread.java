a7e1f75e72047ab10b544c0d781477d310c94def
Robert Z <urdxd@student.kit.edu>
2017-02-02 09:36:35 +0100

    Implemented startExitThread Method.

--- src/curlingBot/main/ExitThread.java
+++ src/curlingBot/main/ExitThread.java
@@ -6,7 +6,13 @@ public class ExitThread extends Thread {
     @Override
     public void run() {
         System.out.println("Press Button to exit.");
-        Button.waitForAnyPress(); // TODO check if this works
+        Button.waitForAnyPress();
         System.exit(0);
+    }
+    
+    public static Thread startExitThread() {
+        Thread exitThread = new Thread(new ExitThread());
+        exitThread.start();
+        return exitThread;
     }
 }
