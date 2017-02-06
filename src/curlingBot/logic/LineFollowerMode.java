509dcbc7b112c8d130ed2be7a3c1da57f87dff5e
Robert Z <urdxd@student.kit.edu>
2017-02-06 12:43:02 +0100

    Added an implementation to the perform method of linefollowermode

--- src/curlingBot/logic/LineFollowerMode.java
+++ src/curlingBot/logic/LineFollowerMode.java
@@ -1,8 +1,13 @@
 package curlingBot.logic;
 
 import curlingBot.main.Globals;
+import curlingBot.motorControl.MoveState;
+import curlingBot.motorControl.PController;
 
 public class LineFollowerMode implements IMoveMode {
+
+    private PController pController;
+    private final int maxAcc = 100; //TODO test acc s
 
     @Override
     public void init() {
@@ -12,9 +17,17 @@ public class LineFollowerMode implements IMoveMode {
         Globals.sensorBuffer.setColorSensorActive(true);
 
     }
-    
+
     @Override
-	public void perform() {
-		
-	}
+    public void perform() {
+        this.pController = new PController();
+        for (;;) {
+            float sensorInput = Globals.sensorBuffer.getLastMessurementColor();
+            float leftSpeed = pController.getSpeedLeft(sensorInput);
+            float rightSpeed = pController.getSpeedRight(sensorInput);
+            MoveState moveState = MoveState.getMoveStateWithLeftAndRightSpeed(leftSpeed, rightSpeed);
+            Globals.motorControl.setMoveState(moveState, maxAcc);
+        }
+
+    }
 }
