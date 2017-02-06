71d18b91a126215f120e10765796b40ba622a9b3
Robert Z <urdxd@student.kit.edu>
2017-02-06 12:13:53 +0100

    Added init method to lineFollowerMode

--- src/curlingBot/logic/LineFollowerMode.java
+++ src/curlingBot/logic/LineFollowerMode.java
@@ -1,8 +1,20 @@
 package curlingBot.logic;
 
+import curlingBot.main.Globals;
+
 public class LineFollowerMode implements IMoveMode {
-	@Override
+
+    @Override
+    public void init() {
+        Globals.sensorBuffer.setGyroSensorActive(true); // for see saw
+        Globals.sensorBuffer.setUltraSonicSensorActive(false);
+        Globals.sensorBuffer.setTouchSensorActive(false); // TODO check
+        Globals.sensorBuffer.setColorSensorActive(true);
+
+    }
+    
+    @Override
 	public void perform() {
-		//Follow the line
+		
 	}
 }
