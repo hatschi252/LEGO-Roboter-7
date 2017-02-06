71ba85c23cd091cb82e88055be20c860cabae6d6
Benjamin Helios <b.helios@t-online.de>
2017-02-06 16:27:08 +0100

    autoFormatiert

--- src/curlingBot/logic/SwingBridgeMode.java
+++ src/curlingBot/logic/SwingBridgeMode.java
@@ -8,21 +8,23 @@ import curlingBot.sensors.SensorBuffer;
 import lejos.utility.Stopwatch;
 
 public class SwingBridgeMode implements IMoveMode {
-	
+
 	private final int TIME_TO_CROSS_BRIDGE = 10; // TODO find out time;
-	
+
 	private PController wallPC;
 	private PController bridgePC;
 	private SensorBuffer sensorBuffer;
 	private MotorControl motorControl;
-	
 
 	@Override
 	public void init() {
 		wallPC = new PController(0.8f, 200, 0.05f, 0.35f, 0.5f);
-		bridgePC = new PController(-1f, 60, 0.05f, 0.30f, 0.5f); //TODO werte genau abmessen
+		bridgePC = new PController(-1f, 60, 0.05f, 0.30f, 0.5f); // TODO werte
+																	// genau
+																	// abmessen
 		sensorBuffer = Globals.sensorBuffer;
-		sensorBuffer.setUltraSonicSensorActive(true); //TODO set all other sensors to false
+		sensorBuffer.setUltraSonicSensorActive(true); // TODO set all other
+														// sensors to false
 		sensorBuffer.setTouchSensorActive(false);
 		sensorBuffer.setGyroSensorActive(false);
 		sensorBuffer.setColorSensorActive(false);
@@ -35,19 +37,17 @@ public class SwingBridgeMode implements IMoveMode {
 		float lastSensorValue;
 		float leftSpeed;
 		float rightSpeed;
-		motorControl.setMoveState(MoveState.getMoveStateWithLeftAndRightSpeed(wallPC.getSpeed0(), wallPC.getSpeed0()), 6000);
-		do
-		{
+		motorControl.setMoveState(MoveState.getMoveStateWithLeftAndRightSpeed(wallPC.getSpeed0(), wallPC.getSpeed0()),
+				6000);
+		do {
 			lastSensorValue = sensorBuffer.getLastMessurementUltraSonic();
 			leftSpeed = wallPC.getSpeedLeft(lastSensorValue);
 			rightSpeed = wallPC.getSpeedRight(lastSensorValue);
 			motorControl.setMoveState(MoveState.getMoveStateWithLeftAndRightSpeed(leftSpeed, rightSpeed), 6000);
-		}
-		while(!Float.isInfinite(lastSensorValue));
+		} while (!Float.isInfinite(lastSensorValue));
 		motorControl.moveUltrasonicDown();
 		Stopwatch timer = new Stopwatch();
-		while (timer.elapsed() < TIME_TO_CROSS_BRIDGE)
-		{
+		while (timer.elapsed() < TIME_TO_CROSS_BRIDGE) {
 			lastSensorValue = sensorBuffer.getLastMessurementUltraSonic();
 			leftSpeed = bridgePC.getSpeedLeft(lastSensorValue);
 			rightSpeed = bridgePC.getSpeedRight(lastSensorValue);
