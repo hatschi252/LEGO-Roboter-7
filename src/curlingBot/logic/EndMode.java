6d8da67fdec7d54549bda3771291e581a5f3d453
Benjamin Helios <b.helios@t-online.de>
2017-02-08 11:47:07 +0100

    EndMode performing EndBossStrategy

--- src/curlingBot/logic/EndMode.java
+++ src/curlingBot/logic/EndMode.java
@@ -1,21 +1,25 @@
 package curlingBot.logic;
 
+import curlingBot.logic.endboss.EndBossStrategy;
 import curlingBot.main.Output;
 
 public class EndMode extends MoveMode {
+	
+	private EndBossStrategy strat;
 
-	public EndMode(String description) {
+	public EndMode(String description, EndBossStrategy strat) {
+		this.strat = strat;
 		super(description);
 	}
 
 	@Override
 	public void init() {
+		strat.init();
 	}
 
 	@Override
 	public void perform() {
-		Output.put("EndMode");
-		Output.finished();
+		strat.perform();
 	}
 	
 
