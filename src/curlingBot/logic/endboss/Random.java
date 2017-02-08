69203c8d9a864cbf39d4e1f0266d0270f5f82f5b
Benjamin Helios <b.helios@t-online.de>
2017-02-08 11:05:52 +0100

    Implemented strategies (interface, random strategy)

---
+++ src/curlingBot/logic/endboss/Random.java
@@ -0,0 +1,64 @@
+package curlingBot.logic.endboss;
+
+import curlingBot.main.Globals;
+import lejos.utility.Delay;
+import lejos.utility.Stopwatch;
+
+public class Random implements EndBossStrategy {
+
+	private final float BRIGHTNESS_THRESH = 0.2f;
+	private final int BACKWARD_SPEED = -200;
+	private final int DRIVE_BACKWARD_DELAY = 1000;
+	private final int STANDARD_SPEED = 400;
+	private final int MIN_TIME = 1500;
+	private final int TIMEINTERVALL = 4000;
+
+	@Override
+	public void init() {
+		Globals.sensorBuffer.setColorSensorActive(true);
+		Globals.sensorBuffer.setGyroSensorActive(false);
+		Globals.sensorBuffer.setTouchSensorActive(true);
+		Globals.sensorBuffer.setUltraSonicSensorActive(true);
+	}
+
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+		// line detection -> turn around
+		// random turn and random drive forwards
+		//
+		while (true) {
+			driveRandomForward();
+		}
+	}
+
+	private void driveRandomForward() {
+		float randomLeftSpeed = (float) (Math.random() * this.STANDARD_SPEED);
+		float randomRightSpeed = (float) (Math.random() * this.STANDARD_SPEED);
+		int randomTime = (int) (Math.random() * this.TIMEINTERVALL + this.MIN_TIME);
+		helperDriveForward(randomLeftSpeed, randomRightSpeed, randomTime);
+	}
+
+	private void helperDriveForward(float leftSpeed, float rightSpeed, int timeToDriveForward) {
+		Stopwatch timer = new Stopwatch();
+		Globals.motorControl.setLeftAndRightSpeed(leftSpeed, rightSpeed);
+		while (timer.elapsed() < timeToDriveForward && !hasLineFound()) {
+			if (Globals.sensorBuffer.getLastMeasurementTouch()) {
+				Globals.motorControl.setLeftAndRightSpeed(this.BACKWARD_SPEED, this.BACKWARD_SPEED);
+				Delay.msDelay(DRIVE_BACKWARD_DELAY); // TODO maybe change non
+														// blocking
+				break;
+			}
+		}
+		if (hasLineFound()) {
+			// robot is over the line return in endboss area (turn around)
+			Globals.motorControl.setLeftAndRightSpeed(this.BACKWARD_SPEED, this.BACKWARD_SPEED);
+			Delay.msDelay(this.DRIVE_BACKWARD_DELAY * 5);
+		}
+	}
+
+	private boolean hasLineFound() {
+		return Globals.sensorBuffer.getLastMessurementColor() > this.BRIGHTNESS_THRESH;
+	}
+
+}
---
+++ src/curlingBot/logic/endboss/StraigtForward.java
@@ -0,0 +1,17 @@
+package curlingBot.logic.endboss;
+
+public class StraigtForward implements EndBossStrategy {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
